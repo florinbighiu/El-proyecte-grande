9097031e02ef05d2d34a4674bfc83b6dc1fe2055
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2023-10-10 11:33:42 +0300

    starter project

---
+++ server/src/main/java/com/codecool/epgsprint1/El/proyecte/grande/ElProyecteGrandeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.epgsprint1.El.proyecte.grande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElProyecteGrandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElProyecteGrandeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codecool/epgsprint1/El/proyecte/grande/ElProyecteGrandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.epgsprint1.El.proyecte.grande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElProyecteGrandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
