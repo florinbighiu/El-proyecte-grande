e1d1acbd15c13e24a04ae25dc1bf3d3c777a65fc
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2023-10-19 14:46:25 +0300

    Minor responsiveness fix on UpdateForm

--- client/src/components/UpdateForm.jsx
+++ client/src/components/UpdateForm.jsx
@@ -3,7 +3,7 @@
 function UpdateForm({ name, price, image, description, handleInputChange, handleUpdate, onClose }) {
     return (
       <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-75 ">
-        <div className="bg-white p-4 rounded-md shadow-md w-1/4">
+        <div className="bg-white p-4 rounded-md shadow-md md:w-1/4">
       <h2 className="text-xl font-semibold mb-2">Update Product</h2>
         <form>
           <div className="mb-4 flex flex-col">
