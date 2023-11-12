fe092f1a6e4f92c69e53a324e9d5d8bf2f8c18e1
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2023-11-12 14:16:38 +0200

    Navbar slight refactor

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -17,22 +17,22 @@ const Navbar = () => {
   const isAuthenticated = localStorage.getItem("authToken");
 
   const content = (
-    <div className="lg:hidden bg-gray-200 block absolute top-14 w-full left-0 right-0  text-black text-bold transition z-50">
+    <div id="content" className="lg:hidden block absolute top-14 w-full left-0 right-0  text-black text-bold transition z-50">
       <ul className="text-center text-xl p-20 ">
-        <li className="my-4 py-4 border-b border-gray-400 hover:bg-gray-300 hover:rounded hover: cursor-pointer">
+        <li className="my-4 py-4 border-b border-gray-400 hover:bg-indigo-300 hover:bg-opacity-25 hover:rounded hover: cursor-pointer">
           <a href="/">Home</a>
         </li>
-        <li className="my-4 py-4 border-b  border-gray-400 hover:bg-gray-300 hover:rounded hover: cursor-pointer">
+        <li className="my-4 py-4 border-b  border-gray-400 hover:bg-indigo-300 hover:bg-opacity-25 hover:rounded hover: cursor-pointer">
           <a href="/products">Products</a>
         </li>
-        <li className="my-4 py-4 border-b  border-gray-400 hover:bg-gray-300 hover:rounded hover: cursor-pointer">
+        <li className="my-4 py-4 border-b  border-gray-400 hover:bg-indigo-300 hover:bg-opacity-25 hover:rounded hover: cursor-pointer">
           <a href="/cart">Cart</a>
         </li>
-        <li className="my-4 py-4 border-b  border-gray-400 hover:bg-gray-300 hover:rounded hover: cursor-pointer">
+        <li className="my-4 py-4 border-b  border-gray-400 hover:bg-indigo-300 hover:bg-opacity-25 hover:rounded hover: cursor-pointer">
           <a href="/contact">Contact</a>
         </li>
         {isAuthenticated ? (
-          <li className="my-4 py-4 border-b  border-gray-400 hover:bg-gray-300 hover:rounded hover: cursor-pointer">
+          <li className="my-4 py-4 border-b  border-gray-400 hover:bg-indigo-300 hover:bg-opacity-25 hover:rounded hover: cursor-pointer">
             <a href="/login" onClick={logout}>
               Logout
             </a>
