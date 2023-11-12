85f388d6c95083726af96b2231f8b4b6e9dd93b1
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2023-11-12 14:29:11 +0200

    StarRating component slight refactor

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -8,7 +8,7 @@ const StarRating = ({ product }) => {
           <svg
             key={index}
             xmlns="http://www.w3.org/2000/svg"
-            className={`text-center w-6 fill-current ${index <= product.rating ? "text-yellow-500" : "text-gray-500"
+            className={`text-center w-7 fill-current ${index <= product.rating ? "text-emerald-600" : "text-gray-500"
               }`}
             viewBox="0 0 24 24">
             <path
