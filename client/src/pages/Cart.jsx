a663324fa47cbc0a219565a48911937df87e629e
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2023-11-13 13:34:19 +0200

    Empty cart element UI refactored

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -101,54 +101,54 @@ function Cart() {
                 {cartProducts.length ? (<>
                     <div className="flex flex-col col-span-2 row-span-1">
                         {cartProducts.map((product) => (
-                        <div
-                            key={product.id}
-                            className="bg-white border border-gray-200/70 bg-opacity-25 backdrop-blur-md lg:h-64 mx-3 my-3 rounded-xl shadow-md  hover:cursor-pointer">
-                            <div className="relative h-full flex flex-col lg:flex-row">
-                                <div className="relative h-full lg:w-1/3">
-                                    <img
-                                        src={product.product.thumbnail}
-                                        alt={product.product.title}
-                                        className="w-full h-full object-fill border border-gray-100/75 shadow-lg rounded-lg"
-                                    />
-                                </div>
+                            <div
+                                key={product.id}
+                                className="bg-white border border-gray-200/70 bg-opacity-25 backdrop-blur-md lg:h-64 mx-3 my-3 rounded-xl shadow-md  hover:cursor-pointer">
+                                <div className="relative h-full flex flex-col lg:flex-row">
+                                    <div className="relative h-full lg:w-1/3">
+                                        <img
+                                            src={product.product.thumbnail}
+                                            alt={product.product.title}
+                                            className="w-full h-full object-fill border border-gray-100/75 shadow-lg rounded-lg"
+                                        />
+                                    </div>
 
-                                <div className="p-8 flex-grow flex justify-evenly flex-col lg:w-1/2">
-                                    <h3 className="text-xl text-black text-center font-semibold font-display mb-2">
-                                        <strong>{product.product.title}</strong>
-                                    </h3>
-                                    <p className="text-lg text-black text-center font-display mb-2">
-                                        {product.product.description}
-                                    </p>
-                                    <p className="text-red-500 text-center font-display text-xl">
-                                        ${product.product.discountPercentage > 0 ? ((product.product.price - (product.product.price * product.product.discountPercentage) / 100) * product.quantity).toFixed(2) : (product.product.price * product.quantity).toFixed(2)}
-                                        {product.product.discountPercentage > 0 && (
-                                            <span className="text-sm text-gray-700 line-through ml-2">
-                                                ${(product.product.price * product.quantity).toFixed(2)}
-                                            </span>)}{" "}
-                                    </p>
-                                    <div className="flex items-center justify-center space-x-1">
-                                        <button disabled={product.quantity < 2} onClick={() => decreaseQuantity(product)}
-                                            className={`mt-2 w-8 h-8 rounded-full bg-pink-100 hover:bg-pink-200 hover:shadow-lg flex justify-center items-center ${product.quantity < 2 ? "bg-slate-100 hover:bg-slate-100 hover:bg-opacity-25 hover:shadow-none bg-opacity-25" : ""}`}>-
-                                        </button>
-                                        <div
-                                            className="mt-2 px-1 rounded-xl w-fit text-center text-2xl font-display">
-                                            <span>{product.quantity}</span>
+                                    <div className="p-8 flex-grow flex justify-evenly flex-col lg:w-1/2">
+                                        <h3 className="text-xl text-black text-center font-semibold font-display mb-2">
+                                            <strong>{product.product.title}</strong>
+                                        </h3>
+                                        <p className="text-lg text-black text-center font-display mb-2">
+                                            {product.product.description}
+                                        </p>
+                                        <p className="text-red-500 text-center font-display text-xl">
+                                            ${product.product.discountPercentage > 0 ? ((product.product.price - (product.product.price * product.product.discountPercentage) / 100) * product.quantity).toFixed(2) : (product.product.price * product.quantity).toFixed(2)}
+                                            {product.product.discountPercentage > 0 && (
+                                                <span className="text-sm text-gray-700 line-through ml-2">
+                                                    ${(product.product.price * product.quantity).toFixed(2)}
+                                                </span>)}{" "}
+                                        </p>
+                                        <div className="flex items-center justify-center space-x-1">
+                                            <button disabled={product.quantity < 2} onClick={() => decreaseQuantity(product)}
+                                                className={`mt-2 w-8 h-8 rounded-full bg-pink-100 hover:bg-pink-200 hover:shadow-lg flex justify-center items-center ${product.quantity < 2 ? "bg-slate-100 hover:bg-slate-100 hover:bg-opacity-25 hover:shadow-none bg-opacity-25" : ""}`}>-
+                                            </button>
+                                            <div
+                                                className="mt-2 px-1 rounded-xl w-fit text-center text-2xl font-display">
+                                                <span>{product.quantity}</span>
+                                            </div>
+                                            <button onClick={() => increaseQuantity(product)}
+                                                className="mt-2 w-8 h-8 rounded-full bg-pink-100 hover:bg-pink-200 hover:shadow-lg flex justify-center items-center">+
+                                            </button>
                                         </div>
-                                        <button onClick={() => increaseQuantity(product)}
-                                            className="mt-2 w-8 h-8 rounded-full bg-pink-100 hover:bg-pink-200 hover:shadow-lg flex justify-center items-center">+
-                                        </button>
-                                    </div>
-                                    <div className="flex items-end justify-center">
-                                        <button
-                                            onClick={() => handleRemoveFromCart(product)}
-                                            className="bg-red-500 hover:bg-red-700 w-2/5 text-white font-bold py-2 px-4 rounded-full mt-2 lg:mt-4 mx-auto lg:mx-0">
-                                            Remove
-                                        </button>
+                                        <div className="flex items-end justify-center">
+                                            <button
+                                                onClick={() => handleRemoveFromCart(product)}
+                                                className="bg-red-500 hover:bg-red-700 w-2/5 text-white font-bold py-2 px-4 rounded-full mt-2 lg:mt-4 mx-auto lg:mx-0">
+                                                Remove
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>))}
+                            </div>))}
                     </div>
                     <div
                         className="h-fit flex items-center justify-start mx-3 my-3 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
@@ -188,15 +188,19 @@ function Cart() {
                             {showCheckout && <Checkout onClose={() => setShowCheckout(false)} />}
                         </div>
                     </div>
-                </>) : (<div className="flex flex-col items-center justify-start col-span-3 mt-12">
-                    <img src={EmptyCart} alt="EmptyCart" className="h-2/4 " />
-                    <h1 className="w-full text-center mt-14 text-lg">
-                        Your cart is empty. To add products to your cart go{" "}
-                        <a href="/products" className="text-blue-500">
-                            back to shop.
-                        </a>
-                    </h1>
-                </div>)}
+                </>) : (
+                    <div className="flex flex-col items-center justify-start col-span-3 mt-8">
+                        <img src={EmptyCart} alt="EmptyCart" className="h-2/5 " />
+                        <h1 className="w-full text-center text-2xl mt-2">
+                            You don't have any items in your cart. Let's get shopping!
+                        </h1>
+                        <div className="h-1/4 flex items-center justify-center">
+                            <a href="/products" className="bg-pink-500 hover:bg-pink-600 font-semibold text-lg text-white rounded-full shadow-md p-3 px-20">
+                                Start shopping
+                            </a>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>);
 }
